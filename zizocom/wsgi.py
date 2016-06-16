b6b167b029c10d538afa7d73e46cd2c8627414c9
masao0127s <msuzuki@mxdlab.net>
2016-06-16 15:23:07 +0900

    Update wsgi.py

--- zizocom/wsgi.py
+++ zizocom/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os, sys
+# ----- 他のアプリと同居させるための設定 -----
+sys.path.append('/var/www/zizocom')
+sys.path.append('/var/www/zizocom/zizocom')
+# ----- 他のアプリと同居させるための設定 -----
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zizocom.settings")
 
 from django.core.wsgi import get_wsgi_application
